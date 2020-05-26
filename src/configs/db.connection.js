6867e6041195d947f54ca61e1c828acb3739e37f
satyamk134 <satyamk134@gmail.com>
2020-05-26 23:29:37 +0530

    connection is mongo atlas added

--- src/configs/db.connection.js
+++ src/configs/db.connection.js
@@ -5,16 +5,14 @@ exports.connectToDb = ()=>{
     /**
      * For local 
     */
-   'mongodb://localhost/'
-   //mongoose.connect('mongodb://'+config.connection.database.host+':'+config.connection.database.port+'/'+config.connection.database.database, {useNewUrlParser: true})
-    /**
-     * For catalog Dev
-    */
-   //console.log("connection string is"+'mongodb://'config.connection.catalog_dev.username+':'+config.connection.catalog_dev.password+'@'+config.connection.catalog_dev.host+':'+config.connection.catalog_dev.port+'/'+config.connection.catalog_dev.database+'?ssl=true')
- 
-   //mongoose.connect('mongodb://'+config.connection.database.host+':'+config.connection.database.port+'/'+config.connection.database.database, {useNewUrlParser: true})
+   
+   
    //console.log("url is",'mongodb://'+config.connection.database.host+':'+config.connection.database.port+'/'+config.connection.database.database)
-   mongoose.connect('mongodb://localhost:27017/forum', {useNewUrlParser: true});
+   mongoose.connect('mongodb+srv://admin:india_123@cluster0-xt9fw.mongodb.net/localshop?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+  });;
+  //mongoose.connect(uri)
+   //mongoose.connect('mongodb://localhost:27017/forum', {useNewUrlParser: true});
    //mongoose.connect('mongodb://user:Qwerty_123@cluster-cataloging-shard-00-02-5gmlz.mongodb.net:27017/catalog-v3?retryWrites=true&ssl=true&authSource=admin')
    mongoose.set('debug', true);
     mongoose.connection.on('connected', function() {
